d751ba274032ec7c02ceb2f95f9b5be83397f026
lilkgb <mcleanrs@comcast.net>
2019-01-28 00:59:13 -0800

    add scripts.js with function addressbook, contact. add prototype addressbook.addcontact, assignid, findcontact. add prototype.contact.fullName

---
+++ js/scripts.js
@@ -0,0 +1,38 @@
+//Business Logic for AddressBook
+function AddressBook() {
+  this.contacts = [];
+  this.currentId = 0;
+}
+
+//Method for addContact
+AddressBook.prototype.addContact = function(contact) {
+  contact.id = this.assignId();
+  this.contacts.push(contact);
+};
+
+//Method to assign a Id to each contact inputed
+AddressBook.prototype.assignId = function() {
+  this.currentId += 1;
+  return this.currentId;
+};
+
+//Business Logic for finding a contact
+AddressBook.prototype.findContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i].id == id) {
+      return this.contacts[i];
+    }
+  }
+  return false;
+};
+
+//Business Logic for Contacts
+function Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName,
+  this.lastName = lastName,
+  this.phoneNumber = phoneNumber
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+};
