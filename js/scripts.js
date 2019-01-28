a772258b1a01bc7a45fffa26f6188d647fe43a49
lilkgb <mcleanrs@comcast.net>
2019-01-28 01:07:30 -0800

    add prototype AddressBook.deleteContact

--- js/scripts.js
+++ js/scripts.js
@@ -26,6 +26,17 @@ AddressBook.prototype.findContact = function(id) {
   return false;
 };
 
+//Business Logic for deleting a contacts
+AddressBook.prototype.deleteContact = function(id) {
+  for (var i=0; i< this.contacts.length; i++) {
+    if (this.contacts[i].id == id) {
+      delete this.contacts[i];
+      return true;
+    }
+  }
+  return false;
+};
+
 //Business Logic for Contacts
 function Contact(firstName, lastName, phoneNumber) {
   this.firstName = firstName,
